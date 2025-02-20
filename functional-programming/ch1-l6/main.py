c579d6a27947f7efaa2b3e0bf5df7090b2d6e85b
filipemarques-dev <filipemarques1995@gmail.com>
2025-02-20 21:00:42 +0000

    chapter 1 lesson 6 exercise from 'Functional Programming' course

---
+++ functional-programming/ch1-l6/main.py
@@ -0,0 +1,17 @@
+def get_median_font_size(font_sizes):
+    
+    list_length = len(font_sizes)
+    sorted_list = sorted(font_sizes)
+    
+    print(sorted_list)
+    
+    if list_length % 2 != 0:
+        return sorted_list[list_length // 2]
+        
+    if len(font_sizes) == 0:
+        return None
+
+    return sorted_list[(list_length // 2) -1]
+
+def get_median_font_size_2(font_sizes):
+    pass
